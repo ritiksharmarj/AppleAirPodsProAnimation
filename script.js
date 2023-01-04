59d958b208b562546eae52ce070e30ad05476b52
ritiksharmarj <bloggeritik@gmail.com>
2023-01-04 21:20:37 +0530

    Final update

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const html = document.documentElement;
 const canvas = document.getElementById('hero-lightpass');
 const context = canvas.getContext('2d');
 
-const frameCount = 148; // images/frames = 147 total (1 -148)
+// images/frames = 147 total images (1 -148)
+const frameCount = 148;
+
+// Fetching the correct images
 const currentFrame = (index) =>
   `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
     .toString()
     .padStart(4, '0')}.jpg`;
 
+// Even better with image preloading
 const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
@@ -23,11 +27,13 @@ img.onload = function () {
   context.drawImage(img, 0, 0);
 };
 
+// Updating <canvas> with the correct image
 const updateImage = (index) => {
   img.src = currentFrame(index);
   context.drawImage(img, 0, 0);
 };
 
+// Connecting images to the user’s scroll progress
 window.addEventListener('scroll', () => {
   const scrollTop = html.scrollTop;
   const maxScrollTop = html.scrollHeight - window.innerHeight;
